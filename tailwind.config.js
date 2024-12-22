9d0db185fe0eca083aee39ae856e22234f65a6cd
nikitaacodes <nikitaa4822@gmail.com>
2024-12-22 18:22:54 +0530

    responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
    'grey': '#454444' ,
    'coral': '#e5af73' ,
    'pinkish' : '#f35566',
-   'primary' : '#6f6ebd' ,
-   'light-brown': '#816c64'  
+   'primary' : '#6f6ebd' , 
+   'primaryb' : '#001A6E' ,
+   'light-brown': '#816c64'  ,
+   'dark-brown' : '#440F50'
     },
       fontFamily: {
         'astyle': ['poppins','sans-serif']
