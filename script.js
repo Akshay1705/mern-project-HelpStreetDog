d60f89e434678b91907701453c26e0073f268870
Nikita Jadhav <114680928+nikitaacodes@users.noreply.github.com>
2025-02-22 14:31:05 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('contactbtn').addEventListener('click', () => {
-    document.getElementById('open').classList.remove('hidden');
-}
-);
-
-document.getElementById('close').addEventListener('click',() => {
-    document.getElementById('open').classList.add('hidden');
-});
